b46b0151527aab50377af71fa84d11b57a75954d
Prratiiik <144805450+Prratiiik@users.noreply.github.com>
2025-03-04 14:53:55 +0000

    Setup for lab

---
+++ lambdas/getAllMovies.ts
@@ -0,0 +1,48 @@
+import { Handler } from "aws-lambda";
+import { DynamoDBClient } from "@aws-sdk/client-dynamodb";
+import { DynamoDBDocumentClient, ScanCommand } from "@aws-sdk/lib-dynamodb";
+
+const ddbDocClient = createDDbDocClient();
+
+export const handler: Handler = async (event, context) => {
+  try {
+    const command = new ScanCommand({
+      TableName: process.env.TABLE_NAME,
+    });
+    const response = await ddbDocClient.send(command);
+    const body = {
+      data: response.Items,
+    };
+
+    return {
+      statusCode: 200,
+      headers: {
+        "content-type": "application/json",
+      },
+      body: JSON.stringify(body),
+    };
+  } catch (error: any) {
+    console.log(JSON.stringify(error));
+    return {
+      statusCode: 500,
+      headers: {
+        "content-type": "application/json",
+      },
+      body: JSON.stringify({ error }),
+    };
+  }
+};
+
+function createDDbDocClient() {
+  const ddbClient = new DynamoDBClient({ region: process.env.REGION });
+  const marshallOptions = {
+    convertEmptyValues: true,
+    removeUndefinedValues: true,
+    convertClassInstanceToMap: true,
+  };
+  const unmarshallOptions = {
+    wrapNumbers: false,
+  };
+  const translateConfig = { marshallOptions, unmarshallOptions };
+  return DynamoDBDocumentClient.from(ddbClient, translateConfig);
+}
